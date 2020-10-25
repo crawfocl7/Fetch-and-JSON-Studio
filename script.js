8a8c493f444641e505e6c01eb28d27157c5d33cd
CCrawford <crawfocl7@gmail.com>
2020-10-24 19:26:38 -0500

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    let astro = "";
+        fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+            response.json().then(function(json) {
+                // const div = document.getElementById('container');
+                json.forEach(element => {
+                // for(let i = 0; i < json.results.length; i++) {
+                astro += `
+
+                <h3>${element.firstName} ${element.lastName}</h3>
+                <li>Hours in space: ${element.hoursInSpace}</li>
+                <li>Active: ${element.active}</li>
+                <li>Skills: ${element.skills}</li>`
+            });
+
+            destination = document.getElementById('container');
+            destination.innerHTML = `<ul>${astro}</ul>`;
+        });
+    });
+});
