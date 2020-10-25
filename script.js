970c60d7633a2f1b25abb54cdaa99c71b0c954eb
CCrawford <crawfocl7@gmail.com>
2020-10-24 22:42:05 -0500

    Reviewed and corrected error

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ window.addEventListener("load", function() {
     let astro = "";
         fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
             response.json().then(function(json) {
-                // const div = document.getElementById('container');
                 json.forEach(element => {
-                // for(let i = 0; i < json.results.length; i++) {
                 astro += `
-
-                <h3>${element.firstName} ${element.lastName}</h3>
-                <li>Hours in space: ${element.hoursInSpace}</li>
-                <li>Active: ${element.active}</li>
-                <li>Skills: ${element.skills}</li>`
+                <div class="astronaut">
+                    <h3>${element.firstName} ${element.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${element.hoursInSpace}</li>
+                        <li>Active: ${element.active}</li>
+                        <li>Skills: ${element.skills.join(', ')}</li>
+                    </ul>
+                    <img src= ${element.picture} class="avatar">
+                </div>`
             });
 
             destination = document.getElementById('container');
-            destination.innerHTML = `<ul>${astro}</ul>`;
+            destination.innerHTML = `${astro}`;
         });
     });
 });
